f2887f7377e4fd08d165ee5cd3dfdf3246101fa8
iruedar <nachete.rueda@gmail.com>
2019-05-02 17:31:52 +0200

    Principiocon foto original

---
+++ filtro.js
@@ -0,0 +1,25 @@
+function main() {
+  console.log("En main()....")
+
+  //-- Acceso al objeto con la imagen
+  var img = document.getElementById('imagesrc')
+
+  //-- Acceso al objeto con el canvas
+  var canvas = document.getElementById('display');
+
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  //-- Obtener el contexto del canvas para
+  //-- trabajar con el
+  var ctx = canvas.getContext("2d");
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imgData.data
+}
